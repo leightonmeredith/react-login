6ab28e8cbb436bac4e60cf10778bae7c16a58213
Leighton Meredith <luluket2000@gmail.com>
2026-06-30 15:30:37 -0400

    add react-router with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 
-import { useState } from "react"
-import LoginForm from "./LoginForm"
-
-
+import { Outlet } from "react-router";
 
 function App() {
-const [myVar, setMyVar]= useState('WORLD')
+  // DECLARATIVE APPROACH
+  // return (
+  //   <BrowserRouter>
+  //     <nav>
+  //       <Link to="/">Home</Link>
+  //       <Link to="/about">About</Link>
+  //     </nav>
 
-function validateLogin() {
-  setMyVar('TEST')
-}
+  //     <Routes>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="/about" element={<About />} />
+  //     </Routes>
+  //   </BrowserRouter>
+  // );
 
   return (
     <>
-    <div className="App">
-      <LoginForm />
-    </div>
-    Hello {myVar}
-    <button className="button" onClick={validateLogin}>
-      CLick
-    </button>
+      <div className="App">
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
